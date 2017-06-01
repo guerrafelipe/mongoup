bd99d1131ad5fb91ba9e97e60ae6888b235835c2
Felipe Guerra <felipeguerrads@gmail.com>
2017-06-01 12:34:53 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import time
+from pymongo import MongoClient
+from urllib import quote_plus
+import json
+from clint.textui import colored
+
+#source = mongodb://app_dev_user:t{;as]N8mUk:27af@54.233.189.15:27017/s2_chat_development
+#target = mongodb://app_prod_user:$G32Zf2d7G'j]]>}@54.233.189.15:27017/s2_chat_production
+
+"""
+Get collections from source
+"""
+
+def import_docs():
+    #source
+    uri = "mongodb://%s:%s@%s" % (quote_plus("root"), quote_plus("t6xEx2LFe3dp"), "54.233.189.15:27017")
+    source_client = MongoClient(uri)
+    source_db = source_client.s2_chat_development
+    source_answers = source_db.answers
+
+    #target
+    target_db = source_client.s2_chat_production
+    target_answers = target_db.answers
+
+    for a in target_answers.find():
+        source_answers.insert_one(a)
+
+def export_docs():
+    """
+    Export to a document
+    """
+    uri = "mongodb://%s:%s@%s" % (quote_plus("root"), quote_plus("t6xEx2LFe3dp"), "54.233.189.15:27017")
+    source_client = MongoClient(uri)
+    source_db = source_client.s2_chat_production
+    source_answers = source_db.answers
+    
+    target_file = open("az_respostas_backup_" + time.strftime("%d-%m-%Y") + ".json", "wb")
+    backup_string, docs_count = "", 0
+    for a in source_answers.find():
+        backup_string += str(a)
+        docs_count += 1
+    target_file.write(backup_string)
+    target_file.close()
+
+    print(colored.green("Success!"))
+    print("->" + colored.blue(str(docs_count)) + " documents imported")
+
+if __name__ == "__main__":
+    export_docs()
